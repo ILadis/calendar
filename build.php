6d36d7e3270ac50950b7136e307b2ab8086b0e92
Michael Wagner <mwagn4@gmail.com>
2019-09-23 06:37:07 +0200

    Checks php.ini settings before building

--- build.php
+++ build.php
@@ -1,6 +1,12 @@
 #!/usr/bin/env php
 <?php
 
+$readonly = boolval(ini_get('phar.readonly'));
+if ($readonly) {
+  echo 'Cannot build Phar when readonly flag is set!';
+  exit(1);
+}
+
 $phar = new Phar('calendar.phar');
 
 $phar->buildFromDirectory(__DIR__, '/vendor/');
